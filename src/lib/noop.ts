e2bbacb4d3bf116b57ff82802fdb28d8768b2d30
Yumii <hei.hajatiana@gmail.com>
2024-01-27 16:09:31 +0300

    feat: add /courses/:cid
    * chore: add markdown cmps
    
    * chore: markdown renderer
    
    * feat: add /courses/:cid

--- src/lib/noop.ts
+++ src/lib/noop.ts
@@ -1,3 +1,11 @@
 export const unimplemented = (): never => {
   throw new Error("Function not implemented");
 };
+
+/**
+ * Function that does nothing that we can use to replace optional fn props to avoid NPE
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const NOOP_FN = (..._args: unknown[]) => {
+  return {} as never;
+};
