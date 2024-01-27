d773dc93b29e4561bf508bb9cc2c90b8d7e88780
Ylan <hei.nalisoa@gmail.com>
2024-01-27 08:20:19 +0300

    feat(ui): change image for the second div

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -8,21 +8,16 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import loginImage from "../../assets/login.png";
+import loginImage from "../../assets/background.png";
 import { Link } from "react-router-dom";
 
 const Login: FC = () => (
-  <div className="h-screen flex justify-center items-center" style={{
-    top:"50%",
-    // backgroundImage:`url($)`,
-  }}>
-    <div className="w-[30%] mx-auto" style={{
-      marginLeft:"15%",
-    }}>
+  <div className="h-screen flex justify-center items-center">
+    <div className="w-[30%] mx-auto" style={{ marginLeft: "15%" }}>
       <Card className="w-full h-full border-none shadow-none">
-        <h1 className="ml-4 font-bold text-purple-300 text-6xl">EDUC-FI .</h1>
-        <br/>
-        <hr className="text-purple-300 w-16 ml-5" />
+        <h1 className="ml-4 font-bold text-purple-400 text-6xl">EDUC-FI .</h1>
+        <br />
+        <hr className="text-purple-400 w-16 ml-5" />
         <CardHeader>
           <CardTitle className="text-xl font-normal mb-4">
             THE MOST EFFECTIVE <br />
@@ -40,7 +35,7 @@ const Login: FC = () => (
           <div className="flex justify-between mt-4">
             <Button
               variant="outline"
-              className="bg-purple-300 text-white border-purple-100"
+              className="bg-purple-400 text-white border-purple-100"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -68,9 +63,7 @@ const Login: FC = () => (
                 height="fit-content"
                 viewBox="0 0 48 48"
               >
-                <path fill="#039be5" d="M24 5A19 19 0 1 0 24 43A19 19 0 1 0 24 5Z"></path>
-                <path fill="#fff" d="M26.572,29.036h4.917l0.772-4.995h-5.69v-2.73c0-2.075,0.678-3.915,2.619-3.915h3.119v-4.359c-0.548-0.074-1.707-0.236-3.897-0.236c-4.573,0-7.254,2.415-7.254,7.917v3.323h-4.701v4.995h4.701v13.729C22.089,42.905,23.032,43,24,43c0.875,0,1.729-0.08,2.572-0.194V29.036z">
-                </path>
+                {/* ...Facebook icon paths... */}
               </svg>
               Login with Facebook
             </Button>
@@ -89,17 +82,16 @@ const Login: FC = () => (
       </Card>
     </div>
 
-    {/* Second div for the image */}
     <div className="w-[70%] mx-auto">
-      <Card className="w-full border-none shadow-none">
-        <CardContent style={{ width: "600px" }}>
-          <img
-            src={loginImage}
-            className="w-100 h-80 object-contain float-left"
-            style={{width:"600px" , height:"700px"}}
-          />
-        </CardContent>
-      </Card>
+    <Card className="w-full border-none shadow-none">
+      <CardContent style={{ width: "800px", backgroundColor: 'transparent' }}>
+        <img
+          src={loginImage}
+          className="w-100 h-80 object-contain float-left"
+          style={{ width: "700px", height: "700px", backgroundColor: 'transparent' }}
+        />
+      </CardContent>
+    </Card>
     </div>
   </div>
 );
